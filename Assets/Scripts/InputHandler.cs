b46d4e9b80463bc1dc05ec41fdf48a46f16f96a7
Donovan Anderson <Don.Anderson1116@gmail.com>
2025-09-16 21:14:52 -0500

    Finished Movement Script
    Movement Script now works

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    public PlayerController CharacterController;
+    private InputAction _moveAction, _lookAction;
+    void Start()
+    {
+        _moveAction = InputSystem.actions.FindAction("Move");
+        _lookAction = InputSystem.actions.FindAction("Look");
+
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movementVector = _moveAction.ReadValue<Vector2>();
+        CharacterController.Move(movementVector);
+
+        Vector2 lookVector = _lookAction.ReadValue<Vector2>();
+        CharacterController.Rotate(lookVector);
+    }
+}
