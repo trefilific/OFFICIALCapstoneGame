013f58120a45eb00280028392399025e89160a40
Donovan Anderson <Don.Anderson1116@gmail.com>
2025-09-25 17:37:58 -0500

    Updated movement script

---
+++ Assets/Scripts/Interactor.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Interactor : MonoBehaviour
+{
+    [SerializeField]
+    private float castDistance = 5f;
+
+    [SerializeField]
+    private Vector3 raycastOffset = new Vector3(0, 1f, 0);
+
+
+}
