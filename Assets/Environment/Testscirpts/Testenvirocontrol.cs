e172b7308fa8382ba93ed71259ea5dd7e0e32a32
trefilific <ntrefbusiness@gmail.com>
2025-09-25 17:43:31 -0500

    Added1stisland

---
+++ Assets/Environment/Testscirpts/Testenvirocontrol.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class Testenvirocontrol : MonoBehaviour
+{
+    private CharacterController _characterController;
+
+    public float MovementSpeed = 10f, RotateSpeed = 5f;
+
+    private float _rotationY;
+
+
+    Rigidbody rb;
+
+    private void Start()
+    {
+
+        _characterController = GetComponent<CharacterController>();
+        // rb = GetComponent<Rigidbody>();
+
+
+    }
+
+    public void Move(Vector2 movementVector)
+    {
+        Vector3 move = transform.forward * movementVector.y + transform.right * movementVector.x;
+        move = move * MovementSpeed * Time.deltaTime;
+        _characterController.Move(move);
+    }
+
+    public void Rotate(Vector2 rotationVector)
+    {
+        _rotationY += rotationVector.x * RotateSpeed * Time.deltaTime;
+        transform.localRotation = Quaternion.Euler(0, _rotationY, 0);
+    }
+    private void Update()
+    {
+    }
+}
+
