3d740b70f56295234936ab3b82e112463fbe24ad
Donovan Anderson <Don.Anderson1116@gmail.com>
2025-11-20 15:24:06 -0600

    Re-Fixed Movement System

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+/*using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InputHandler : MonoBehaviour
@@ -29,5 +29,54 @@ public class InputHandler : MonoBehaviour
     private void OnJumpPerformed(InputAction.CallbackContext context)
     {
         CharacterController.Jump();
+    }
+}*/
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] private PlayerController CharacterController;
+    [SerializeField] private InputActionAsset inputActions; // Reference to your Input Actions asset
+
+    private InputAction _moveAction, _lookAction, _jumpAction;
+
+    void Start()
+    {
+        // Get the action map (replace "Player" with your actual action map name)
+        var actionMap = inputActions.FindActionMap("Player");
+
+        _moveAction = actionMap.FindAction("Move");
+        _lookAction = actionMap.FindAction("Look");
+        _jumpAction = actionMap.FindAction("Jump");
+
+        // Enable the action map
+        actionMap.Enable();
+
+        _jumpAction.performed += OnJumpPerformed;
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        Vector2 movementVector = _moveAction.ReadValue<Vector2>();
+        CharacterController.Move(movementVector);
+
+        Vector2 lookVector = _lookAction.ReadValue<Vector2>();
+        CharacterController.Rotate(lookVector);
+    }
+
+    private void OnJumpPerformed(InputAction.CallbackContext context)
+    {
+        CharacterController.Jump();
+    }
+
+    private void OnDestroy()
+    {
+        // Clean up
+        if (_jumpAction != null)
+            _jumpAction.performed -= OnJumpPerformed;
     }
 }
