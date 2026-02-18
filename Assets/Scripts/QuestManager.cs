fac6ae471c7d1d0e601301ae963be97d23cdc53b
Donovan Anderson <Don.Anderson1116@gmail.com>
2026-02-17 18:25:23 -0600

    Initial QuestManager creation

--- Assets/Scripts/QuestManager.cs
+++ Assets/Scripts/QuestManager.cs
@@ -1,8 +1,31 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class QuestManager : MonoBehaviour
 {
+    [SerializeField] ScriptableObject questItem;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public void KillObjective(ScriptableObject scriptableObject)
+    {
+        string enemyName = scriptableObject.name;
+        int requiredKills = 5; 
+    }
+
+    public void QuestDefinition(ScriptableObject scriptableObject)
+    {
+      string questName = scriptableObject.name;
+      string rewardInfo = "Reward: 100 gold coins";
+      
+      List<string> objectives = new List<string>
+      {
+          "Kill 5 Goblins",
+          "Collect 10 Herbs",
+          "Defeat the Goblin King"
+      };
+    }
     void Start()
     {
         
@@ -11,6 +34,6 @@ public class QuestManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+      Instantiate(questItem);
     }
 }
