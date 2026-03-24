ba25ae726c3d9618b523cd20638aa79942736d19
PaceyJ <143014092+PaceyJ@users.noreply.github.com>
2026-03-24 01:20:25 -0500

    Minimap logic and test/some inventory stuff

---
+++ Assets/Items/Inventory.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+
+    public void AddItem(Item item)
+    {
+        items.Add(item);
+        Debug.Log("Picked up: " + item.itemName);
+    }
+}
